651209389e6a6e2e7af58949715c0625c2f6665d
Anton A S <eddclyde@yandex.ru>
2026-02-05 22:46:27 +0300

    fix[]: fixed hotkey for plan mode

--- extensions/plan-mode/index.ts
+++ extensions/plan-mode/index.ts
@@ -113,7 +113,7 @@ export default function planModeExtension(pi: ExtensionAPI): void {
     },
   });
 
-  pi.registerShortcut(Key.shift("p"), {
+  pi.registerShortcut(Key.ctrlAlt("p"), {
     description: "Toggle plan mode",
     handler: async (ctx) => togglePlanMode(ctx),
   });
